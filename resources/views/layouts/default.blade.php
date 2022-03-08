1461884810351306e5ff101a5e7fbbcf1e376adf
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-03-08 15:47:47 +0100

    Footer is now at the bottom
    the footer is at the bottom where it belongs

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -36,14 +36,15 @@
 
         </div>
 
-        <div class="row">
-            @include('includes.footer')
-        </div>
+        
 
     </div>
 
 
 </div>
+        <div class="row">
+            @include('includes.footer')
+        </div>
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
